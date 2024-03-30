453105423499d70c8c8917198ec3c953af568523
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2024-03-30 21:42:25 +0000

    Add landing page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import HeaderWelcome from "$lib/components/HeaderWelcome.svelte";
+  import viewerImage from "$lib/images/viewer-demo-01.png"
+</script>
+
+
+<HeaderWelcome></HeaderWelcome>
+<div class="hero min-h-[calc(100vh-70px)] bg-base-200">
+  <div class="hero-content flex-col lg:flex-row-reverse">
+    <img src="{viewerImage}" class="max-w-xl rounded-lg shadow-2xl" alt="Sitevisor Viewer"/>
+    <div>
+      <h1 class="text-5xl font-bold">Welcome to SiteVisor!</h1>
+      <p class="py-6">SiteVisor is a Digital Twin based web app for environmental monitoring of buildings, with elements of asset management.</p>
+      <a class="btn btn-primary" href="/register">Register now</a>
+    </div>
+  </div>
+</div>
